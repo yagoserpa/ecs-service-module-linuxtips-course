a3e6beb378e980811004cb4abec14ca84eb06194
Yago Serpa <yago.serpa@intive.com.ar>
2024-10-13 04:09:34 -0300

    Add module configuration.

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {}
+variable "service_name" {}
+variable "service_port" {}
+variable "service_cpu" {}
+variable "service_memory" {}
+variable "service_listener" {}
+variable "service_task_execution_role" {}
+variable "service_healthcheck" {
+  type = map(any)
+}
+variable "service_launch_type" {}
+variable "service_task_count" {}
+variable "service_hosts" {}
+variable "cluster_name" {}
+variable "vpc_id" {}
+variable "private_subnets" {}
+variable "environment_variables" {
+  type = list(any)
+}
+variable "capabilities" {
+  type = list(any)
+}
